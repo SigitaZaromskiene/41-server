5a3c6721b1fe3d6122e1bbbff647acaa160a91bf
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-19 13:31:05 +0200

    App init

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("la");
+import { server } from "./lib/server.js";
+const app = {};
+
+app.init = () => {
+  console.log("pradedam");
+  server.init();
+};
+
+app.init();
+
+export { app };
