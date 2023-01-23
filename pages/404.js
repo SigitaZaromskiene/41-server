d231448bfcf312c4d7b0bfda66dceb0069cfd1ef
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-23 18:33:17 +0200

    PageTemplate

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import { PageTemplate } from "../lib/PageTemplate.js";
+
+class Page404 extends PageTemplate {}
+
+export { Page404 };
---
+++ pages/about.js
@@ -0,0 +1,5 @@
+import { PageTemplate } from "../lib/PageTemplate.js";
+
+class PageAbout extends PageTemplate {}
+
+export { PageAbout };
