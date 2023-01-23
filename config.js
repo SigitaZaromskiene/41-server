e13aabcac7d1d0bcdf6627bd5ab8395f3669b00f
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-01-23 16:53:45 +0200

    Config

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {};
+
+config.dev = {
+  name: "dev",
+  port: 41069,
+};
+
+config.prod = {
+  name: "prod",
+  port: 41070,
+};
+
+console.log(process.env.NODE_ENV);
+const envName = process.env.NODE_ENV;
+const env = config[envName] ? config[envName] : config.dev;
+
+export default env;
